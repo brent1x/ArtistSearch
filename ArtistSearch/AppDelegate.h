2f96e995a5d8f07963034e1d6c09a1aacf34889c
Brent Dady <brentdady@gmail.com>
2015-11-11 11:02:54 -0600

    Initial Commit

---
+++ ArtistSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArtistSearch
+//
+//  Created by Brent Dady on 11/11/15.
+//  Copyright © 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArtistSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArtistSearch
+//
+//  Created by Brent Dady on 11/11/15.
+//  Copyright © 2015 Brent Dady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
